09aabe8d4197e68ca52f962d28e9d2f0f687ac7b
rrc_w2024_sarbjitsingh <156253849+sarbjit2554@users.noreply.github.com>
2025-01-29 21:01:52 -0600

    entered a code

---
+++ src/api/v1/controllers/employee.controller.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from "express";
+import * as EmployeeService from "../services/employee.service";
+
+export const createEmployee = (req: Request, res: Response) => {
+  const employee = EmployeeService.createEmployee(req.body);
+  res.status(201).json(employee);
+};
+
+export const getAllEmployees = (req: Request, res: Response) => {
+  res.json(EmployeeService.getAllEmployees());
+};
+
+export const getEmployeeById = (req: Request, res: Response) => {
+  const employee = EmployeeService.getEmployeeById(req.params.id);
+  employee ? res.json(employee) : res.status(404).json({ message: "Employee not found" });
+};
+
+export const updateEmployee = (req: Request, res: Response) => {
+  const updatedEmployee = EmployeeService.updateEmployee(req.params.id, req.body);
+  updatedEmployee ? res.json(updatedEmployee) : res.status(404).json({ message: "Employee not found" });
+};
+
+export const deleteEmployee = (req: Request, res: Response) => {
+  const success = EmployeeService.deleteEmployee(req.params.id);
+  success ? res.json({ message: "Employee deleted" }) : res.status(404).json({ message: "Employee not found" });
+};
