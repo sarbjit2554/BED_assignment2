649e002a49c42eca299833020076096625c3220e
rrc_w2024_sarbjitsingh <156253849+sarbjit2554@users.noreply.github.com>
2025-01-29 21:06:10 -0600

    created employee.ts

---
+++ src/api/v1/interfaces/employee.ts
@@ -0,0 +1,10 @@
+export interface Employee {
+    id: string;
+    name: string;
+    position: string;
+    department: string;
+    email: string;
+    phone: string;
+    branchId: string;
+  }
+  
